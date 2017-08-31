731b82dd1efa9017843c249046fc147a51a029cc
ashique <ashique12009@gmail.com>
2017-09-01 00:38:43 +0600

    Initial commit

---
+++ taka_currency.php
@@ -0,0 +1,37 @@
+<?php
+
+echo bd_taka_format(123456789.52);
+
+function bd_taka_format( $amount = 0 )
+{
+	$tmp 		= explode( ".", $amount ); // for float or double values
+	$strMoney 	= "";
+	$isNegetive = false;
+	if ( $amount < 0 ) {
+	    $amount 		= substr( $tmp[0], 1 );
+	    $is_negetive 	= true;
+	}
+	else {
+	    $amount 		= $tmp[0];
+	}
+    
+	$strMoney 	.= substr( $amount, -3, 3 ) ;
+	$amount 	= substr( $amount, 0, -3 ) ;
+
+	while ( strlen( $amount ) > 0 ) {
+		$strMoney 	= substr( $amount, -2, 2 ).",".$strMoney;
+		$amount 	= substr( $amount, 0, -2 );
+	}
+
+	if ( isset($tmp[1]) ) { // if float and double add the decimal digits here.
+	    if ( $is_negetive ) {
+	        return "-".$strMoney.".".$tmp[1];
+	    }
+		return $strMoney.".".$tmp[1];
+	}
+	
+	if ( $is_negetive ) {
+	    return "-".$strMoney;
+	}
+	return $strMoney;
+}
